d37b69244883e13cf00c238681c34ad1cb3309d3
Hao Xu <hxuu0069@student.monash.edu>
2022-08-08 19:28:44 +1000

    add week 03 files

---
+++ week03/FIT5032_MyCodeSnippet/FIT5032_MyCodeSnippet/Models/Exercise/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_MyCodeSnippet.Models.Exercise
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Student(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
+
+    public class ExampleDictionary
+    {
+        public void Example()
+        {
+            Dictionary<int, Student> students = new Dictionary<int, Student>();
+
+            Student s1 = new Student("Steve", "");
+            Student s2 = new Student("James", "");
+
+            students.Add(1, s1);
+            students.Add(2, s2);
+
+            Student result = new Student("", "");
+
+            students.TryGetValue(1, out result);
+
+        }
+    }
+}
---
+++ week03/FIT5032_MyCodeSnippet/FIT5032_MyCodeSnippet/Models/HelloWorld/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_MyCodeSnippet.Models.HelloWorld
+{
+    public class Hello
+    {
+        public string GetHello()
+        {
+            return "Hello, World!";
+        }
+    }
+
+    public class HelloWorld 
+    { 
+    }
+}
