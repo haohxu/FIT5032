d4ae4d7164bd974e8eed37dfacfec8d13f54a71c
Hao Xu <hxuu0069@student.monash.edu>
2022-09-18 10:28:35 +1000

    modify week 07 files

---
+++ week07/FIT5032_MyIdentity/SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Students] (
+	[Id] INT IDENTITY (1, 1) NOT NULL, 
+	[FirstName] NVARCHAR (max) NOT NULL, 
+	[LastName] NVARCHAR (max) NOT NULL, 
+	[UserId] NVARCHAR (max) NOT NULL
+	PRIMARY KEY (Id)
+);
+GO
+
+CREATE TABLE [dbo].[Units] (
+	[Id] INT IDENTITY (1, 1) NOT NULL, 
+	[Name] NVARCHAR (max) NOT NULL, 
+	[Description] NVARCHAR (max) NOT NULL, 
+	[StudentId] INT NOT NULL
+	PRIMARY KEY (Id),
+	FOREIGN KEY (StudentId) REFERENCES Students (Id)
+);
+GO
