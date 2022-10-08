c7150312974cf3faa01cba4b0dea39051f718b37
Hao Xu <hxuu0069@student.monash.edu>
2022-10-08 23:04:46 +1100

    add week 10 files

---
+++ week10/FIT5032_SimpleAngularApp/FIT5032_SimpleAngularApp/Models/UnitContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FIT5032_SimpleAngularApp.Models
+{
+    public class UnitContext : DbContext
+    {
+        public UnitContext(DbContextOptions<UnitContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Unit> Units { get; set; } = null!;
+    }
+}
